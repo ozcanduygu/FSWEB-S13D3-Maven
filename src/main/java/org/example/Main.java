9ce50ffaf98a9fa7267ccf4a95f16701e03bb7a7
ozcanduygu <duyguuozcann@hotmail.com>
2025-12-24 22:03:10 +0300

    oop giriş

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Person person1 = new Person("John", "Doe", 25);
+        System.out.println(person1.getFirstName());
+        System.out.println(person1.isTeen());
+        System.out.println((person1.getLastName()));
+
+        Person person2 = new Person("Duygu", "Özcan", 32, true, 1.65, 54);
+
+        System.out.println("KadınMı: "+ true);
+
+
+        Wall wall1 = new Wall(23,10);
+        System.out.println("Area: "+wall1.getArea() );
     }
+
+
 }
